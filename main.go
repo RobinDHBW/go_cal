accc0b21711e863d41705c4ea15c43fb653c6ee4
Timo <timo.max2002@gmail.com>
2022-12-22 14:56:07 +0100

    initial html + css

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
 
+type data struct {
+	Name  string
+	Email string
+}
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	q := r.URL.Query()
+	name := q.Get("name")
+
+	d := data{
+		Name:  name,
+		Email: "noreply@xyz.de",
+	}
+	var tempInit = template.Must(template.ParseFiles("./templates/test.html"))
+	tempInit.Execute(w, d)
+}
 func main() {
-	fmt.Println("hello world")
+	http.HandleFunc("/", mainHandler)
+	http.Handle("/templates/static/", http.StripPrefix("/templates/static", http.FileServer(http.Dir("templates/static"))))
+	log.Fatalln(http.ListenAndServe(":8080", nil))
 }
