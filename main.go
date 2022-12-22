ca1a560892a314e60cf690e6d9d277417797cc34
Marco-374 <77583303+Marco-374@users.noreply.github.com>
2022-12-23 00:09:17 +0100

    Basic Calendar View

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"go_cal/calendarView"
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
 
 type data struct {
@@ -12,15 +14,16 @@ type data struct {
 }
 
 func mainHandler(w http.ResponseWriter, r *http.Request) {
-	q := r.URL.Query()
-	name := q.Get("name")
+	//q := r.URL.Query()
+	//name := q.Get("name")
 
-	d := data{
-		Name:  name,
-		Email: "noreply@xyz.de",
+	cal := calendarView.Calendar{
+		Month:   time.Now().Month(),
+		Year:    time.Now().Year(),
+		Current: time.Now(),
 	}
-	var tempInit = template.Must(template.ParseFiles("./templates/test.html"))
-	tempInit.Execute(w, d)
+	var tempInit = template.Must(template.ParseFiles("./templates/test.tmpl.html"))
+	tempInit.Execute(w, cal)
 }
 func main() {
 	http.HandleFunc("/", mainHandler)
