d0e1ab6423bf17e249620f3100ddba54d00e0b2e
Timo <timo.max2002@gmail.com>
2022-12-23 11:51:59 +0100

    calenderView finished (beautiful)

--- main.go
+++ main.go
@@ -8,15 +8,7 @@ import (
 	"time"
 )
 
-type data struct {
-	Name  string
-	Email string
-}
-
 func mainHandler(w http.ResponseWriter, r *http.Request) {
-	//q := r.URL.Query()
-	//name := q.Get("name")
-
 	cal := calendarView.Calendar{
 		Month:   time.Now().Month(),
 		Year:    time.Now().Year(),
@@ -25,8 +17,10 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 	var tempInit = template.Must(template.ParseFiles("./templates/test.tmpl.html"))
 	tempInit.Execute(w, cal)
 }
+
 func main() {
 	http.HandleFunc("/", mainHandler)
+	http.HandleFunc("/updateCalendar", calendarView.UpdateCalendarHandler)
 	http.Handle("/templates/static/", http.StripPrefix("/templates/static", http.FileServer(http.Dir("templates/static"))))
 	log.Fatalln(http.ListenAndServe(":8080", nil))
 }
