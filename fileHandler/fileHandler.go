5b9e78730cc7b8e7f097fce3df3e04b509703879
Robin Wollenschlaeger <r.wollenschlaeger@goettfert.de>
2022-12-23 23:35:19 +0100

    add id, userlevel to user struct, add structs and blank methods to fileHandler class, write blank tests for tdd

--- fileHandler/fileHandler.go
+++ fileHandler/fileHandler.go
@@ -1,1 +1,41 @@
 package fileHandler
+
+type Appointment struct {
+	DateTime    string `json:"dateTime"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Userid      int    `json:"userid"`
+	UserLevel   int    `json:"userLevel"`
+	Timeseries  struct {
+		Repeat    bool `json:"repeat"`
+		Intervall int  `json:"intervall"`
+	} `json:"timeseries"`
+	Share struct {
+		Public bool   `json:"public"`
+		Url    string `json:"url"`
+	} `json:"share"`
+}
+
+type User struct {
+	UserName     string `json:"userName"`
+	Password     string `json:"password"`
+	Id           int    `json:"id"`
+	Appointments []Appointment
+}
+
+type FileHandler struct {
+	fileNames []string
+}
+
+// Initialize structs from disk
+func New() FileHandler {
+	return FileHandler{}
+}
+
+func (fh FileHandler) SyncToFile(user User) {
+
+}
+
+func (fh FileHandler) ReadFromFile(user User) {
+
+}
--- fileHandler/fileHandler_test.go
+++ fileHandler/fileHandler_test.go
@@ -1,1 +1,15 @@
 package fileHandler
+
+import (
+	"testing"
+)
+
+// Check to write json struct to file and readback --> struct must be same
+func TestFileHandler_SyncToFile(t *testing.T) {
+
+}
+
+// Check to read json from file
+func TestFileHandler_ReadFromFile(t *testing.T) {
+
+}
