ebd6dd3ca2e78fa2a1b041bc8f1283cdd9349b6d
Robin Wollenschlaeger <r.wollenschlaeger@goettfert.de>
2022-12-24 13:39:11 +0100

    add dataModel constructor + test

--- dataModel/dataModel.go
+++ dataModel/dataModel.go
@@ -1,13 +1,30 @@
 package dataModel
 
 import (
+	"encoding/json"
+	"go_cal/data"
 	"go_cal/fileHandler"
 )
 
 //Class to hold all data and coordinate sync to/from file
 
 type DataModel struct {
-	UserList []fileHandler.User
+	UserList []data.User
+	fH       fileHandler.FileHandler
+}
+
+func NewDM(dataPath string) DataModel {
+	fH := fileHandler.NewFH(dataPath)
+	sList := fH.ReadAll()
+	var uList []data.User
+
+	for _, uString := range sList {
+		var user data.User
+		json.Unmarshal([]byte(uString), &user)
+		uList = append(uList, user)
+	}
+
+	return DataModel{uList, fH}
 }
 
 func (dm DataModel) getUser(id int) {
@@ -18,7 +35,11 @@ func (dm DataModel) addUser() {
 
 }
 
-//Call by referenceor call by value?
-func (dm DataModel) addAppointment(id int, ap fileHandler.Appointment) {
+// Call by reference or call by value?
+func (dm DataModel) addAppointment(id int, ap data.Appointment) {
+
+}
+
+func (dm DataModel) deleteAppointment(id int) {
 
 }
--- dataModel/dataModel_test.go
+++ dataModel/dataModel_test.go
@@ -1,1 +1,40 @@
 package dataModel
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"go_cal/data"
+	"go_cal/fileHandler"
+	"testing"
+)
+
+var uList = []data.User{data.NewUser("test1", "test", 1), data.NewUser("test2", "test", 2), data.NewUser("test3", "test", 3)}
+
+func fileWriteRead(user data.User, fH fileHandler.FileHandler) data.User {
+	write, err := json.Marshal(user)
+	if err != nil {
+		panic(err)
+	}
+	fH.SyncToFile(write, user.Id)
+
+	fString := fH.ReadFromFile(1)
+
+	var rUser data.User
+	json.Unmarshal([]byte(fString), &rUser)
+	return rUser
+}
+
+func init() {
+	fH := fileHandler.NewFH("../data/test")
+	for _, uD := range uList {
+		fileWriteRead(uD, fH)
+	}
+}
+
+func TestNewDM(t *testing.T) {
+	dP := "../data/test"
+	dM := NewDM(dP)
+
+	//Check if dataPath correct and UserList correct
+	assert.EqualValues(t, uList, dM.UserList)
+}
