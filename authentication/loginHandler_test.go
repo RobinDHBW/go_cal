2b99325c789c9740caea37e1cffcfc2336de477e
Timo <timo.max2002@gmail.com>
2022-12-27 15:27:20 +0100

    created logoutHandler_test

--- authentication/loginHandler_test.go
+++ authentication/loginHandler_test.go
@@ -184,37 +184,6 @@ func TestLoginHandlerWithValidCookie(t *testing.T) {
 //	assert.NotEmpty(t, users)
 //}
 
-func TestLogoutHandler(t *testing.T) {
-	deleteAllUsers()
-	deleteAllSessions()
-
-	// create user
-	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte("test123"), bcrypt.DefaultCost)
-	users["testUser"] = hashedPassword
-	// create session
-	sessionToken, _ := createSession("testUser")
-
-	// TODO: http und localhost
-	request, _ := http.NewRequest(http.MethodPost, "http://localhost:8080/logout", nil)
-	request.AddCookie(&http.Cookie{
-		Name:  "session_token",
-		Value: sessionToken,
-	})
-	response := httptest.NewRecorder()
-	http.HandlerFunc(LogoutHandler).ServeHTTP(response, request)
-
-	assert.Empty(t, sessions)
-
-	assert.Equal(t, http.StatusFound, response.Result().StatusCode)
-	locationHeader, err := response.Result().Location()
-	assert.NoError(t, err)
-	assert.Equal(t, "/", locationHeader.Path)
-
-	assert.Equal(t, "session_token", response.Result().Cookies()[0].Name)
-	assert.Equal(t, "", response.Result().Cookies()[0].Value)
-	assert.LessOrEqual(t, response.Result().Cookies()[0].Expires, time.Now())
-}
-
 func TestRegisterHandler(t *testing.T) {
 	deleteAllUsers()
 	deleteAllSessions()
