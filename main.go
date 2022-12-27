70be9cfa82c012023da4898530862cd3af40e9e1
Marco-374 <77583303+Marco-374@users.noreply.github.com>
2022-12-27 01:41:51 +0100

    terminlist zeigt termine an

--- main.go
+++ main.go
@@ -10,12 +10,21 @@ import (
 	"time"
 )
 
+var globalTemp = 0
+
 func mainHandler(w http.ResponseWriter, r *http.Request) {
 	cal := calendarView.Calendar{
 		Month:   time.Now().Month(),
 		Year:    time.Now().Year(),
 		Current: time.Now(),
 	}
+	if globalTemp == 0 { // nur zum Testen
+		terminHandling.TView.TList.CreateTermin("T1", "1 content", time.Now().AddDate(0, 0, -1), time.Now())
+		terminHandling.TView.TList.CreateTermin("T2", "2 content", time.Now(), time.Now())
+		terminHandling.TView.TList.CreateTermin("T3", "3 content", time.Now().AddDate(0, 0, -2), time.Now())
+	}
+	globalTemp = 1
+
 	var tempInit = template.Must(template.ParseFiles("./templates/test.tmpl.html"))
 	tempInit.Execute(w, cal)
 }
@@ -26,6 +35,7 @@ func main() {
 	http.HandleFunc("/login", authentication.LoginHandler)
 	http.HandleFunc("/register", authentication.RegisterHandler)
 	http.HandleFunc("/terminlist", terminHandling.TerminHandler)
+	http.HandleFunc("/updateTerminList", terminHandling.TerminHandler)
 	http.Handle("/templates/static/", http.StripPrefix("/templates/static", http.FileServer(http.Dir("templates/static"))))
 	log.Fatalln(http.ListenAndServe(":8080", nil))
 }
--- terminHandling/terminView.go
+++ terminHandling/terminView.go
@@ -4,6 +4,8 @@ import (
 	"go_cal/calendarView"
 	"go_cal/templates"
 	"net/http"
+	"sort"
+	"strconv"
 	"time"
 )
 
@@ -32,32 +34,39 @@ type TerminList struct {
 type TerminView struct {
 	TList         TerminList
 	TerminPerSite int
+	TerminSite    int
 	MinDate       time.Time
 }
 
+var Tlist = TerminList{}
+
 var TView = TerminView{
-	TerminList{},
+	Tlist,
 	7,
+	1,
 	time.Now(),
 }
 
 func TerminHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
+
 	switch {
 	case r.Form.Has("calendarBack"):
 		templates.TempInit.Execute(w, calendarView.Cal)
-	case r.Form.Has("dateChoose"):
+	case r.Form.Has("submitTermin"):
+		TView.TerminPerSite, _ = strconv.Atoi(r.Form.Get("numberPerSite"))
+		TView.TerminSite, _ = strconv.Atoi(r.Form.Get("siteChoose"))
+		TView.MinDate, _ = time.Parse("2006-01-02", r.Form.Get("dateChoose"))
+		TView.GetTerminList()
 		templates.TempTerminList.Execute(w, TView)
-	case r.Form.Has("siteChoose"):
+	default:
 		templates.TempTerminList.Execute(w, TView)
-	case r.Form.Has("numberPerSite"):
-		templates.TempTerminList.Execute(w, TView)
+
 	}
-	templates.TempTerminList.Execute(w, TView)
 
 }
 
-func (tl TerminList) CreateTermin(title string, content string, begin time.Time, end time.Time) {
+func (tl *TerminList) CreateTermin(title string, content string, begin time.Time, end time.Time) {
 	termin := Termin{
 		Title:     title,
 		Content:   content,
@@ -65,5 +74,25 @@ func (tl TerminList) CreateTermin(title string, content string, begin time.Time,
 		End:       end,
 		Repeating: None,
 	}
-	TView.TList.Termine = append(TView.TList.Termine, termin)
+	tl.Termine = append(tl.Termine, termin)
+}
+
+func (tv TerminView) GetTerminList() []Termin {
+	sort.SliceStable(tv.TList.Termine, func(i, j int) bool {
+		return tv.TList.Termine[i].Begin.Before(tv.TList.Termine[j].Begin)
+	})
+
+	datefilteredTL := make([]Termin, 0, 1)
+	for i := range tv.TList.Termine {
+		if tv.MinDate.Before(tv.TList.Termine[i].Begin) || tv.MinDate.Equal(tv.TList.Termine[i].Begin) {
+			datefilteredTL = append(datefilteredTL, tv.TList.Termine[i])
+		}
+	}
+	if tv.TerminPerSite*(tv.TerminSite-1) > len(datefilteredTL) {
+		return nil
+	}
+	if tv.TerminSite*tv.TerminPerSite > len(datefilteredTL) {
+		return datefilteredTL[tv.TerminPerSite*(tv.TerminSite-1):]
+	}
+	return datefilteredTL[tv.TerminPerSite*(tv.TerminSite-1) : tv.TerminSite*tv.TerminPerSite]
 }
