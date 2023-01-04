154e376931e305c235e0097b86ffc2878a45c314
Robin Wollenschlaeger <r.wollenschlaeger@goettfert.de>
2023-01-04 22:40:17 +0100

    working test for icalhandler

--- terminHandling/terminExport_test.go
+++ terminHandling/terminExport_test.go
@@ -1,1 +1,60 @@
 package terminHandling
+
+import (
+	"github.com/stretchr/testify/assert"
+	"go_cal/dataModel"
+	"go_cal/export"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+const dataPath = "../data/test/ICAL"
+
+func after1() {
+	err := os.RemoveAll(dataPath)
+	if err != nil {
+		return
+	}
+}
+
+func createServer(auth export.AuthenticatorFunc) *httptest.Server {
+	return httptest.NewServer(
+		export.Wrapper(auth,
+			func(w http.ResponseWriter, r *http.Request) {
+				ICalHandler(w, r)
+			}))
+}
+
+func TestICalHandler(t *testing.T) {
+	dataModel.InitDataModel(dataPath)
+	user, err := dataModel.Dm.AddUser("test", "abc", 3)
+	if err != nil {
+		t.FailNow()
+	}
+
+	defer after()
+
+	//var _, _ string
+	ts := createServer(func(name, pwd string) bool {
+		//receivedName = name
+		//receivedPwd = pwd
+		return true // <--- accept any request
+	})
+	defer ts.Close()
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", ts.URL, nil)
+	assert.NoError(t, err)
+	req.SetBasicAuth("test", "abc")
+	res, err := client.Do(req)
+	assert.NoError(t, err)
+	body, err := io.ReadAll(res.Body)
+
+	//create mock
+	ics := export.NewICal(dataModel.Dm.GetAppointmentsForUser(user.Id)).ToString()
+	assert.EqualValues(t, []byte(ics), body)
+
+}
