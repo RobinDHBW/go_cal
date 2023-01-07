9837ba24aa432a4c183cde114e84e8c462103f9f
Robin Wollenschlaeger <r.wollenschlaeger@goettfert.de>
2023-01-07 15:47:41 +0100

    fix test

--- configuration/flags_test.go
+++ configuration/flags_test.go
@@ -14,6 +14,6 @@ func TestReadFlags(t *testing.T) {
 	assert.EqualValues(t, 8080, flag.Lookup("port").Value.(flag.Getter).Get().(int))
 	assert.EqualValues(t, 10, flag.Lookup("timeout").Value.(flag.Getter).Get().(int))
 	assert.EqualValues(t, "./files", flag.Lookup("folder").Value.(flag.Getter).Get().(string))
-	assert.EqualValues(t, "./", flag.Lookup("certPath").Value.(flag.Getter).Get().(string))
-	assert.EqualValues(t, "./", flag.Lookup("keyPath").Value.(flag.Getter).Get().(string))
+	assert.EqualValues(t, "./server.crt", flag.Lookup("certPath").Value.(flag.Getter).Get().(string))
+	assert.EqualValues(t, "./server.key", flag.Lookup("keyPath").Value.(flag.Getter).Get().(string))
 }
